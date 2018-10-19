800b31545b691f9d6bf3eece6844239c12454dca
ilafila <bob300@inbox.ru>
2018-10-19 20:45:54 +0300

    commit to master

--- homework2/factorial.py
+++ homework2/factorial.py
@@ -1,113 +1,87 @@
 from memory_profiler import profile, time
 from unittest import TestCase, main
 import sys
+
+
 @profile
 def factorial(a):
-    fac=1
-    if (a==0):
+    fac = 1
+    if a == 0:
         return 1
-    if (type(a) != int) or a<0:
-        return "ValueError"
-    for i in range(a):
-        fac *=(i+1)
+    if type(a) != int or a < 0:
+        raise ValueError
+    for i in range(1, a + 1):
+        fac *= i
     return fac
 
 
-
-
-#chislo1=int(input())
-#print(factorial(b))
-
 @profile
-def startRec(b):
+def start_rec(b):
     sys.setrecursionlimit(100000)
     return factorial_rec(b)
 
+
 def factorial_rec(b):
-    if (b==0):
+    if b == 0:
         return 1
-    if (type(b) != int) or b<0:
-        return "ValueError"
+    if (type(b) != int) or b < 0:
+        raise ValueError
     return 1 if b == 1 else b * factorial_rec(b - 1)
 
 
-#chislo=int(input())
+class Tests(TestCase):
+    def test1(self):
+        self.assertRaises(ValueError, factorial, '7')
+
+    def test2(self):
+        self.assertRaises(ValueError, factorial, "ab")
+
+    def test3(self):
+        self.assertRaises(ValueError, factorial, 9.0)
+
+    def test4(self):
+        self.assertEqual(factorial(10), 3628800)
+
+    def test5(self):
+        self.assertEqual(factorial(0), 1)
+
+    def test6(self):
+        self.assertRaises(ValueError, factorial, -2)
+
+    def test7(self):
+        self.assertRaises(ValueError, factorial, 10.5)
+
+    def test8(self):
+        self.assertRaises(ValueError, factorial, [7])
+
+    def test9(self):
+        self.assertRaises(ValueError, start_rec, "7")
+
+    def test10(self):
+        self.assertRaises(ValueError, start_rec, "ab")
+
+    def test11(self):
+        self.assertRaises(ValueError, start_rec, 9.0)
+
+    def test12(self):
+        self.assertEqual(start_rec(10), 3628800)
+
+    def test13(self):
+        self.assertEqual(start_rec(0), 1)
+
+    def test14(self):
+        self.assertRaises(ValueError, start_rec, -2)
+
+    def test15(self):
+        self.assertRaises(ValueError, start_rec, 10.5)
+
+    def test16(self):
+        self.assertRaises(ValueError, start_rec, [7])
+
+
 wtime = time.process_time()
-factorial(10)
-print(time.process_time() - wtime )
-
-
-
-
-
-
-class Validator(TestCase):
-    def test_correct_values(self):
-        if not self.assertEqual(factorial("7"), "ValueError"):
-            print("Test passed")
-        else:
-            print("Test failed")
-        if not self.assertEqual(factorial("ab"), "ValueError"):
-            print("Test passed")
-        else:
-            print("Test failed")
-        if not self.assertEqual(factorial(9.0), "ValueError"):
-            print("Test passed")
-        else:
-            print("Test failed")
-        if not self.assertEqual(factorial(10), 3628800):
-            print("Test passed")
-        else:
-            print("Test failed")
-        if not self.assertEqual(factorial(0), 1):
-            print("Test passed")
-        else:
-            print("Test failed")
-        if not self.assertEqual(factorial(-2), "ValueError"):
-            print("Test passed")
-        else:
-            print("Test failed")
-        if not self.assertEqual(factorial(10.5), "ValueError"):
-            print("Test passed")
-        else:
-            print("Test failed")
-        if not self.assertEqual(factorial([7]), "ValueError"):
-            print("Test passed")
-        else:
-            print("Test failed")
-        if not self.assertEqual(startRec("7"), "ValueError"):
-            print("Test passed")
-        else:
-            print("Test failed")
-        if not self.assertEqual(startRec("ab"), "ValueError"):
-            print("Test passed")
-        else:
-            print("Test failed")
-        if not self.assertEqual(startRec(9.0), "ValueError"):
-            print("Test passed")
-        else:
-            print("Test failed")
-        if not self.assertEqual(startRec(10), 3628800):
-            print("Test passed")
-        else:
-            print("Test failed")
-        if not self.assertEqual(startRec(0), 1):
-            print("Test passed")
-        else:
-            print("Test failed")
-        if not self.assertEqual(startRec(-2), "ValueError"):
-            print("Test passed")
-        else:
-            print("Test failed")
-        if not self.assertEqual(startRec(10.5), "ValueError"):
-            print("Test passed")
-        else:
-            print("Test failed")
-        if not self.assertEqual(startRec([7]), "ValueError"):
-            print("Test passed")
-        else:
-            print("Test failed")
-
-
+factorial(3)
+print(time.process_time() - wtime)
+start_rec(3)
+print(time.process_time() - wtime)
 main()
-
--- homework2/pascal_tree.py
+++ homework2/pascal_tree.py
@@ -1,45 +1,34 @@
 import math
 from unittest import TestCase, main
-def Pascal_tree(number_of_lines):
+
+
+def pascal_tree(number_of_lines):
     if type(number_of_lines) != int or number_of_lines < 0:
-        return "ValueError"
+        raise ValueError
     new_list = [[1]]
-    for n in range(1,number_of_lines+1):
-        origin_list=[]
-        for k in range(0,n+1):
-            origin_list.append(math.factorial(n) / (math.factorial(k) * math.factorial(n - k)))
+    for n in range(1, number_of_lines+1):
+        origin_list = []
+        for k in range(0, n + 1):
+            origin_list.append(math.factorial(n) // (math.factorial(k) * math.factorial(n - k)))
         new_list.append(origin_list)
     return new_list
 
 
-b=int(input())
-ans = Pascal_tree(b)
-for i in range(len(ans)):
-    print(ans[i])
-
-
 class Validator(TestCase):
-    def test_correct_values(self):
-        if not self.assertEqual(Pascal_tree('abc'), "ValueError"):
-            print("Test passed")
-        else:
-            print("Test failed")
-        if not self.assertEqual(Pascal_tree(0), [[1]]):
-            print("Test passed")
-        else:
-            print("Test failed")
-        if not self.assertEqual(Pascal_tree(2), [[1], [1, 1], [1, 2, 1]]):
-            print("Test passed")
-        else:
-            print("Test failed")
-        if not self.assertEqual(Pascal_tree(-3), "ValueError"):
-            print("Test passed")
-        else:
-            print("Test failed")
-        if not self.assertEqual(Pascal_tree(4), [[1], [1, 1], [1, 2, 1], [1, 3, 3, 1], [1, 4, 6, 4, 1]]):
-            print("Test passed")
-        else:
-            print("Test failed")
+    def test1(self):
+        self.assertRaises(ValueError, pascal_tree, 'abc')
+
+    def test2(self):
+        self.assertEqual(pascal_tree(0), [[1]])
+
+    def test3(self):
+        self.assertEqual(pascal_tree(2), [[1], [1, 1], [1, 2, 1]])
+
+    def tesst4(self):
+        self.assertRaises(ValueError, pascal_tree, -3)
+
+    def test5(self):
+        self.assertEqual(pascal_tree(4), [[1], [1, 1], [1, 2, 1], [1, 3, 3, 1], [1, 4, 6, 4, 1]])
 
 
 main()
