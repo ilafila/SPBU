0a9831725d35ab6916d52cc42148ba22285593f8
ilafila <bob300@inbox.ru>
2018-10-27 00:49:24 +0300

    commit

--- homework3/caesar_logic.py
+++ homework3/caesar_logic.py
@@ -1,13 +1,13 @@
 def encrypt(offset, text):
     storage = []
     result = ''
+    if offset < 0:
+        offset = 26 + offset
     if type(offset) != int:
         raise TypeError
     for i in text:
         if ord(i) < ord('A') or ord(i) > ord('z'):
             new_number = ord(i)
-        elif ord(i) < ord('A') or ord(i) > ord('z') and offset < 0:
-            new_number = ord(i) - offset
         else:
             new_number = ord(i) + offset
         storage.append(new_number)
@@ -22,23 +22,4 @@ def encrypt(offset, text):
 
 
 def decrypt(offset, text):
-    storage = []
-    result = ''
-    if type(offset) != int:
-        raise TypeError
-    for i in text:
-        if ord(i) < ord('A') or ord(i) > ord('z'):
-            new_number = ord(i)
-        elif ord(i) < ord('A') or ord(i) > ord('z') and offset < 0:
-            new_number = ord(i) + offset
-        else:
-            new_number = ord(i) + offset
-        storage.append(new_number)
-    for j in range(len(storage)):
-        if storage[j] > ord('Z') and (storage[j] < (ord('a') + offset)):
-            result += chr(storage[j] - 26)
-        elif storage[j] > ord('z'):
-            result += chr(storage[j] - 26)
-        else:
-            result += chr(storage[j])
-    return result
+    return encrypt(-offset, text)
